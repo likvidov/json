6e31f849ec992d040da1c4373bec6141f0ff37f9
likvidov <likvidya@gmail.comm>
2023-07-16 18:28:27 +0300

    Отправка данных через XMLHttpRequest

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-const url = "db.json";
+// const url = "db.json";
 const url2 = 'https://jsonplaceholder.typicode.com/posts';
 
-const getData = async url => {
-  return fetch(url)
-    .then(response => response.json())
-    .catch(error => console.log(error))
+// const getData = async url => {
+//   return fetch(url)
+//     .then(response => response.json())
+//     .catch(error => console.log(error))
+// }
+
+// const sendData = async (url, user) => {
+//   return fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json;charset=utf-8'
+//     },
+//     body: user
+//   }).then(response => response.json())
+//     .catch(error => console.log(error))
+// }
+
+// getData(url)
+//   .then(data => console.log(data));
+
+// getData(url)
+//   .then(data => sendData(url2, JSON.stringify(data)))
+//   .then(data => console.log(data))
+
+let user = {
+  "user": "Vlad",
+  "age": 33,
+  "role": "developer"
 }
 
-const sendData = async (url, user) => {
-  return fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: user
-  }).then(response => response.json())
-    .catch(error => console.log(error))
-}
+let xhr = new XMLHttpRequest();
 
-getData(url)
-  .then(data => console.log(data));
+xhr.open('POST', url2);
+xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+xhr.send(JSON.stringify(user));
+xhr.onload = () => console.log(xhr.response);
 
-getData(url)
-  .then(data => sendData(url2, JSON.stringify(data)))
-  .then(data => console.log(data))
